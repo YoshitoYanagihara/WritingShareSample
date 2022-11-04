f10d6dee33fa98b416a9cd4c7242b1c90ada0587
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-11-05 00:18:19 +0900

    受信したデータをとりあえずconsoleに吐き出すようにした

--- server/index.js
+++ server/index.js
@@ -3,8 +3,7 @@ const WebSocketServer = require("ws").Server
 const server = new WebSocketServer({ port: 3000 })
 
 server.on("connection", socket => {
-  console.log("Connected!")
-  socket.on("close", () => {
-    console.log("Closed...")
+  socket.on("message", data => {
+    console.log(data)
   })
 })
