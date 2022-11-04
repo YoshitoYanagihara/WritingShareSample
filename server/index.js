b6bf23ac66bff6047c87043c6afa56814c756da2
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-11-05 00:25:22 +0900

    とりあえずブロードキャストするだけ

--- server/index.js
+++ server/index.js
@@ -3,7 +3,15 @@ const WebSocketServer = require("ws").Server
 const server = new WebSocketServer({ port: 3000 })
 
 server.on("connection", socket => {
-  socket.on("message", data => {
-    console.log(data)
+  const broadcast = (data) => {
+    server.clients.forEach(s => {
+      if (s !== socket) {
+        s.send(data)
+      }
+    })
+  }
+  
+  socket.on("message", msg => {
+    broadcast(msg)
   })
 })
