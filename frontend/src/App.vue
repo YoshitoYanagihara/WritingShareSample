572473d0aa669546ef29eec5950ce2b199b11196
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-11-04 22:26:09 +0900

    DynamicDialog準備

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import DynamicDialog from "primevue/dynamicdialog"
 </script>
 
 <template lang="pug">
 .root
+DynamicDialog
 </template>
 
 <style lang="sass" scoped>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,9 +8,12 @@ import "primevue/resources/themes/saga-blue/theme.css"
 import "primevue/resources/primevue.min.css"
 import "primeicons/primeicons.css"
 import PrimeVue from "primevue/config"
+import DialogService from "primevue/dialogservice"
+app.use(PrimeVue)
+app.use(DialogService)
+
 import PrimeButton from "primevue/button"
 import InputText from "primevue/inputtext"
-app.use(PrimeVue)
 app.component("PrimeButton", PrimeButton)
 app.component("InputText", InputText)
 
