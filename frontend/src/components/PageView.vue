5e7f37063469ec526d40c68b6a192eb4d4f551e3
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-11-04 22:41:47 +0900

    紙面になるものを配置

--- frontend/src/components/PageView.vue
+++ frontend/src/components/PageView.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
+import { ref } from "vue"
+
+const pageCount = ref(1)
 </script>
 
 <template lang="pug">
 div
   .root
+    .page
+      span Page: {{ pageCount }}
 </template>
 
 <style lang="sass" scoped>
+.root
+  width: 95vw
+  height: 95vh
+  .page
+    display: flex
+    justify-content: center
+    align-items: center
+    width: 100%
+    height: 100%
 </style>
