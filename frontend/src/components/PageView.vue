abddb1a9dfad4d049c2032f2a88bc5f9624158cc
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-11-04 22:49:43 +0900

    改ページボタン

--- frontend/src/components/PageView.vue
+++ frontend/src/components/PageView.vue
@@ -2,6 +2,17 @@
 import { ref } from "vue"
 
 const pageCount = ref(1)
+
+/**
+ * 前のページに戻る
+ */
+const onPrevPage = () => {
+  pageCount.value = Math.max(1, pageCount.value - 1)
+}
+
+const onNextPage = () => {
+  pageCount.value = Math.min(pageCount.value + 1, 10)
+}
 </script>
 
 <template lang="pug">
@@ -9,16 +20,38 @@ div
   .root
     .page
       span Page: {{ pageCount }}
+    .leftButton
+      PrimeButton(@click="onPrevPage")
+    .rightButton
+      PrimeButton(@click="onNextPage")
 </template>
 
 <style lang="sass" scoped>
 .root
-  width: 95vw
+  position: relative
+  width: 99vw
   height: 95vh
+
   .page
     display: flex
     justify-content: center
     align-items: center
     width: 100%
     height: 100%
+
+  .leftButton
+    position: absolute
+    left: 0
+    top: 0
+    height: 100%
+    display: flex
+    align-items: center
+
+  .rightButton
+    position: absolute
+    right: 0
+    top: 0
+    height: 100%
+    display: flex
+    align-items: center
 </style>
